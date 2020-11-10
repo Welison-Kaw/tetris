6ae4244ab323baa2fd2213e9b74d18c4b66154f7
Welison-Kaw <welisoncomw@gmail.com>
2020-11-09 23:44:26 -0300

    Criação de teste da função de tetriminos O e  L. Criada função que posiciona um bloco no grid dados os valores x, y.

--- tetris.py
+++ tetris.py
@@ -1,24 +1,61 @@
 import pygame
 from pygame.locals import *
 
+def grid_position(t):
+	return (t[0]*block_size+t[0]+1,t[1]*block_size+t[1]+2)
+
+def tetrimino_o(type):
+	if type == COLOR:
+		return (255,255,0)
+	if type == SHAPE:
+		return ((0,0)
+			   ,(0,1)
+			   ,(1,0)
+			   ,(1,1))
+
+def tetrimino_l(type):
+	if type == COLOR:
+		return (255,165,0)
+	if type == SHAPE:
+		return ((0,0)
+			   ,(0,1)
+			   ,(0,2)
+			   ,(1,2))
+
+COLOR = 0
+SHAPE = 1
+
 pygame.init()
 max_x = 640
 max_y = 480
 block_size = 20
 screen = pygame.display.set_mode((max_x, max_y))
 
-cell = pygame.Surface((19, 19))
-cell.fill((0,0,0))
-
 matrix = pygame.Surface((block_size*10+11,block_size*20+22))
 matrix.fill((255,255,255))
 matrix_pos = ((max_x-(block_size*10+11))/2,15)
 
+print(grid_position((1,2)))
+
+# cria cada item cell
+cell = []
 for i in range(10):
+	cell.append([])
 	for j in range(20):
-		matrix.blit(cell, (i*block_size+i+1,j*block_size+j+2))
+		cell[i].append([])
+		cell[i][j] = pygame.Surface((block_size, block_size))
+		cell[i][j].fill((0,0,0))
+
+cell[2][2].fill((255,0,0))
 
 pygame.display.set_caption('Tetris')
+
+cell[2][2].fill((255,0,0))
+
+mino = pygame.Surface((block_size, block_size))
+mino.fill(tetrimino_l(COLOR))
+tetrimino = tetrimino_l(SHAPE)
+tetrimino_pos = (30,30)
 
 while True:
 	for event in pygame.event.get():
@@ -36,6 +73,16 @@ while True:
 			quit()
 
 	screen.fill((0,0,0))
+
+	for i in range(10):
+		for j in range(20):
+			# matrix.blit(cell[i][j], (i*block_size+i+1,j*block_size+j+2))
+			matrix.blit(cell[i][j], grid_position((i,j)))
+
+	for pos in tetrimino:
+		# print(pos*block_size)
+		matrix.blit(mino,grid_position(pos))
+
 	screen.blit(matrix, matrix_pos)
 
 	# pygame.draw.rect(screen,(50,50,50),[300,2,100,40])

78c3e23e928d0a2b48235e98d316bb06112d8f4b
Welison-Kaw <welisoncomw@gmail.com>
2020-11-20 20:47:37 -0300

    criada classe pra representar o tetrimino

---
+++ classes/tetrimino.py
@@ -0,0 +1,46 @@
+from my_constants import *
+
+
+class Tetrimino:
+    def __init__(self, _type):
+        self.type = _type
+        # self.__color = get_color
+        # self.__shape = get_shape
+
+    def get_color(self):
+        switcher = {
+            O_SHAPE: [0xFF, 0xFF, 0x00],  # yellow
+            I_SHAPE: [0x00, 0xBF, 0xFF],  # deep sky blue
+            T_SHAPE: [0x80, 0x00, 0x80],  # purple
+            L_SHAPE: [0xFF, 0xA5, 0x00],  # orange
+            J_SHAPE: [0x1E, 0x32, 0xFF],  # blue
+            S_SHAPE: [0x00, 0x80, 0x00],  # green
+            Z_SHAPE: [0xFF, 0x00, 0x00]   # red
+        }
+        return switcher.get(self.type, "Invalid Shape")
+
+    def get_shape(self):
+        switcher = {
+            O_SHAPE: [[1, 1],
+                      [1, 1]],
+            I_SHAPE: [[0, 0, 0, 0],
+                      [1, 1, 1, 1],
+                      [0, 0, 0, 0],
+                      [0, 0, 0, 0]],
+            T_SHAPE: [[0, 1, 0],
+                      [1, 1, 1],
+                      [0, 0, 0]],
+            L_SHAPE: [[0, 0, 1],
+                      [1, 1, 1],
+                      [0, 0, 0]],
+            J_SHAPE: [[1, 0, 0],
+                      [1, 1, 1],
+                      [0, 0, 0]],
+            S_SHAPE: [[0, 1, 1],
+                      [1, 1, 0],
+                      [0, 0, 0]],
+            Z_SHAPE: [[1, 1, 0],
+                      [0, 1, 1],
+                      [0, 0, 0]]
+        }
+        return switcher.get(self.type, "Invalid Shape")

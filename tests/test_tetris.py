36eba909aab845732d2fcc8f1192333740be39b2
Welison-Kaw <welisoncomw@gmail.com>
2020-11-20 19:08:38 -0300

    retirado teste

--- tests/test_tetris.py
+++ tests/test_tetris.py
@@ -1,10 +1,11 @@
-# from tetris import tetrimino_giver
+from myFunctions import tetrimino_giver
 
 
 def test_collision():
+    print("\n.abc")
     assert True
 
 
-# def test_tetrimino_giver():
-#     tetrimino = tetrimino_giver(0)
-#     assert (tetrimino == [[255, 255, 0], [1, 1, 1, 1]])
+def test_tetrimino_giver():
+    tetrimino = tetrimino_giver(0)
+    assert (tetrimino == ([255, 255, 0], [[1, 1], [1, 1]]))

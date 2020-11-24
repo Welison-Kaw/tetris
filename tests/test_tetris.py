4e65ef8e2e2b0f8635eb8974505ce0f897280f51
Welison-Kaw <welisoncomw@gmail.com>
2020-11-23 23:35:17 -0300

    Alterado o test de movimentação do tetrimino, usando agora o método da classe

--- tests/test_tetris.py
+++ tests/test_tetris.py
@@ -1,4 +1,5 @@
 from my_functions import *
+from classes.tetrimino import Tetrimino
 
 
 def test_collision():
@@ -6,56 +7,18 @@ def test_collision():
 
 
 def test_tetrimino_giver():
-    tetrimino = tetrimino_giver(0)
-    assert (tetrimino == ([255, 255, 0], [[1, 1], [1, 1]]))
+    tetrimino = Tetrimino(0)
+    assert (tetrimino.get_color() == [255, 255, 0])
+    assert tetrimino.get_shape() == [[1, 1], [1, 1]]
 
 
-def test_move_right():
-    position_x = 9
-    position_y = 0
-    shape = [[1, 1], [1, 1]]
-
-    if not collision(tetrimino_mover(reshape(shape), position_x + 1, position_y), WALL):
-        position_x += 1
-    assert(position_x == 9)
-
-    position_x = 8
-    if not collision(tetrimino_mover(reshape(shape), position_x + 1, position_y), WALL):
-        position_x += 1
-    assert(position_x == 8)
-
-    position_x = 7
-    if not collision(tetrimino_mover(reshape(shape), position_x + 1, position_y), WALL):
-        position_x += 1
-    assert(position_x == 8)
-
-    position_y = 10
-    position_x = 7
-    if not collision(tetrimino_mover(reshape(shape), position_x + 1, position_y), WALL):
-        position_x += 1
-    assert(position_x == 8)
-
-
-def test_move_left():
-    position_x = 0
-    position_y = 0
-    shape = [[1, 1], [1, 1]]
-    if not collision(tetrimino_mover(reshape(shape), position_x - 1, position_y), WALL):
-        position_x -= 1
-    assert(position_x == 0)
-
-    position_x = 1
-    if not collision(tetrimino_mover(reshape(shape), position_x - 1, position_y), WALL):
-        position_x -= 1
-    assert(position_x == 0)
-
-    position_x = 2
-    if not collision(tetrimino_mover(reshape(shape), position_x - 1, position_y), WALL):
-        position_x -= 1
-    assert(position_x == 1)
-
-    position_x = 2
-    position_y = 10
-    if not collision(tetrimino_mover(reshape(shape), position_x - 1, position_y), WALL):
-        position_x -= 1
-    assert(position_x == 1)
+def test_tetrimino_move():
+    tetrimino = Tetrimino(2)
+    assert tetrimino.position() == [[4, 0], [3, 1], [4, 1], [5, 1]]
+    tetrimino.move_right()
+    assert tetrimino.position() == [[5, 0], [4, 1], [5, 1], [6, 1]]
+    tetrimino.move_right()
+    assert tetrimino.position() == [[6, 0], [5, 1], [6, 1], [7, 1]]
+    tetrimino.move_left()
+    assert tetrimino.position() == [[5, 0], [4, 1], [5, 1], [6, 1]]
+    assert True

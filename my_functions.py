70cd7c130ad5a3dd6f088bd96c6f3591f5a0ff58
Welison-Kaw <welisoncomw@gmail.com>
2020-11-25 00:04:04 -0300

    Excluída a função mover (já está como método)

--- my_functions.py
+++ my_functions.py
@@ -5,13 +5,6 @@ def grid_position(t):
     return t[0] * BLOCK_SIZE + t[0] + 1, t[1] * BLOCK_SIZE + t[1] + 2
 
 
-def tetrimino_mover(_shape, _column, _line):
-    for i in range(len(_shape)):
-        _shape[i][0] += _column
-        _shape[i][1] += _line
-    return _shape
-
-
 # transforma numa estrutura que o pygame consiga escrever
 def reshape(old_shape):
     r = []

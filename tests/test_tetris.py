fcb79fbea9e124bedf472fd6d8171745a27c7a6e
Welison-Kaw <welisoncomw@gmail.com>
2020-11-27 00:37:42 -0300

    Novo teste

--- tests/test_tetris.py
+++ tests/test_tetris.py
@@ -1,4 +1,6 @@
+from classes.matrix import Matrix
 from classes.tetrimino import Tetrimino
+import pygame
 
 
 def test_collision():
@@ -27,3 +29,27 @@ def test_tetrimino_move():
     tetrimino.move_right()
     tetrimino.move_right()
     assert tetrimino.position() == [[8, 0], [7, 1], [8, 1], [9, 1]]
+
+
+def test_ghost_position():
+    matrix = Matrix(pygame, 20, 400)
+
+    matrix.cell[9][19].color = (0xFF, 0xFF, 0x00)
+    matrix.cell[8][19].color = (0xFF, 0xFF, 0x00)
+    matrix.cell[9][18].color = (0xFF, 0xFF, 0x00)
+    matrix.cell[8][18].color = (0xFF, 0xFF, 0x00)
+
+    tetrimino = Tetrimino(2)
+    tetrimino.move_right()
+    tetrimino.move_right()
+    tetrimino.move_right()
+    tetrimino.move_right()
+    tetrimino.move_right()
+    tetrimino.move_right()
+
+    print("\n.{}".format(tetrimino.position()))
+    print("\n.{}".format(tetrimino.ghost_position()))
+
+    assert tetrimino.ghost_position() == [[8, 16], [7, 17], [8, 17], [9, 17]]
+
+    pass
